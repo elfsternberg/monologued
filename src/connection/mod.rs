40c60c299e4f4ff6b4cf663d52656a01d11ef9da
Elf M. Sternberg <elf.sternberg@gmail.com>
2017-11-12 09:30:36 -0800

    Moving along.

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,16 +1,129 @@
-use std::net::{SocketAddr};
+use unicode_segmentation::UnicodeSegmentation;
+
 use mio::tcp::{TcpStream};
+use mio::Event;
+use std::io::{Read, Write};
+use std::collections::VecDeque;
+use bytes::{Buf, BufMut, BytesMut};
+use mio::Ready;
+
+
+pub enum ConnectionState {
+    Running,
+    Done
+}
 
 pub struct Connection {
-    pub client_addr: SocketAddr,
-    pub handle: TcpStream,
+    pub socket: TcpStream,
+    buf: BytesMut,
+    res: VecDeque<Vec<u8>>,
+    done: bool,
+    state: ConnectionState,
 }
 
 impl Connection {
-    pub fn new(handle: TcpStream, address: SocketAddr) -> Self {
+    pub fn new(socket: TcpStream) -> Self {
         Connection {
-            client_addr: address,
-            handle: handle
+            buf: BytesMut::with_capacity(512),
+            res: VecDeque::new(),
+            socket: socket,
+            done: false,
+            state: ConnectionState::Running,
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        !self.done
+    }
+
+    // Take the event and decide what to do with it.
+    pub fn handle(&mut self, event: &Event) -> bool {
+        if self.is_running() && (event.readiness() & Ready::writable() == Ready::writable()) {
+            self.write();
+        }
+        if self.is_running() && (event.readiness() & Ready::readable() == Ready::readable()) {
+            self.read();
+        }
+        return self.is_running()
+    }
+
+    // Notified that there is a desire to read, we read as much
+    // as we can, copying the kernel buffer into the program
+    // buffer (ugh), then notifying the protocol if there's
+    // data to be read.  If anything goes wrong, we notify
+    // the connection that something has failed.
+    fn read(&mut self) -> bool {
+        loop {
+            let (len, ret) = {
+                let mut buf = unsafe { &mut BytesMut::bytes_mut(&mut self.buf) };
+                let len = buf.len();
+                let ret = self.socket.read(buf);
+                (len, ret)
+            };
+            match ret {
+                Ok(0) => break,
+                Ok(l) => {
+                    unsafe { self.buf.advance_mut(l); }
+                    if l != len || BufMut::remaining_mut(&self.buf) == 0 {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    self.done = true;
+                    return false;
+                }
+            }
+        }
+        let c = self.buf.clone();
+        loop {
+            match c.iter().position(|&x| x == b'\n' || x == b'\r') {
+                Some(p) => {
+                    let word = String::from_utf8_lossy(&c[0..p]);
+                    let drow: String = word
+                        .graphemes(true)
+                        .rev()
+                        .flat_map(|g| g.chars())
+                        .collect();
+                    self.res.push_back(drow.into_bytes());
+                }
+                None => { break; }
+            }
+        }
+        true
+    }
+
+    fn write(&mut self) -> bool {
+        if self.res.len() == 0 {
+            return true
+        }
+
+        loop {
+            let mut buf = self.res.pop_front();
+            
+            loop {
+                
+                let (len, res) = {
+                    let ibuf = &buf.bytes();
+                    let len = ibuf.len();
+                    let res = self.socket.try_write(ibuf);
+                    (len, res)
+                };
+                match res {
+                    Ok(None) => {
+                        break;
+                    },
+                    Ok(Some(r)) => {
+                        Buf::advance(&mut buf, r);
+                        if r != len || Buf::remaining_mut(buf) == 0 {
+                            break;
+                        }
+                    },
+                    Err(_) => {
+                        Buf::advance(&mut buf, len);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 extern crate mio;
-    
+extern crate bytes;
+extern crate unicode_segmentation;
+
 use mio::*;
 use mio::tcp::{TcpListener};
-
-use std::io::{Write};
 use std::collections::HashMap;
+use mio::unix::UnixReady;
 
 const MAX_CONNECTIONS: usize = 1024;
 const LISTENER: mio::Token = mio::Token(0);
@@ -21,6 +22,7 @@ fn main() {
     let mut count = 1;
 
     poll.register(&listener, LISTENER, Ready::readable(), PollOpt::edge()).unwrap();
+    println!("Listening on {}", address.port());
                   
     loop {
         poll.poll(&mut events, None).unwrap();
@@ -31,28 +33,31 @@ fn main() {
                         Ok((stream, client_addr)) => (stream, client_addr),
                         Err(e) => panic!("Got an error when accepting a connection: {}", e)
                     };
+                    // TODO: Replace with a self-incrementing priority queue
                     count += 1;
                     println!("Connection from {}", client_addr);
-                    poll.register(&stream, Token(count), Ready::readable() | Ready::writable(), PollOpt::edge())
+                    poll.register(&stream,
+                                  Token(count),
+                                  Ready::readable() |
+                                      Ready::writable() |
+                                      Ready::from(UnixReady::hup()),
+                                  PollOpt::edge())
                         .unwrap();
-                    let conn = Connection::new(stream, client_addr);
-                    connections.insert(count, conn);
+                    connections.insert(count, Connection::new(stream));
                 }
+
+
                 Token(c) => {
                     println!("Got token {}", c);
-                    {
+                    let done = {
                         let conn = connections.get_mut(&c).unwrap();
-                        if event.readiness().is_writable() {
-                            let message = format!("Hello, {}\r\n", conn.client_addr).into_bytes();
-                            match conn.handle.write(&message) {
-                                Ok(_) => println!("Wrote to client {}", conn.client_addr),
-                                Err(e) => println!("Error when writing to connection {}", e)
-                            }
-                        }
+                        conn.handle(&event)
+                    };
+
+                    if done {
+                        let conn = connections.remove(&c).unwrap();
+                        poll.deregister(&conn.socket);
                     }
-                    
-                    let conn = connections.remove(&c).unwrap();
-                    poll.deregister(&conn.handle);
                 }
             }
         }
